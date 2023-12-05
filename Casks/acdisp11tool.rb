ef49099550ac079fa2da95b54a7826e65acdb9e8
AustriaCard <office@austriacard.at>
2023-12-05 16:19:29 +0100

    Updated Cask

--- Casks/acdisp11tool.rb
+++ Casks/acdisp11tool.rb
@@ -1,6 +1,6 @@
 cask "acdisp11tool" do
   version "1.0.0"
-  sha256 "b692c1b27cf12f9f25de73470e44b735b7d62262b7a2e52ce6f6a1fab5e62ca7"
+  sha256 "1105f64581f3befb973956f703331af5401a101a649304feeda9c50b07dcbcda"
   url "https://github.com/MarkusPunz12/ACDIS-PKCS11/releases/download/1.0.0/mac-p11tool-1.0.0.tar.gz", verified: "github.com/MarkusPunz12/ACDIS-PKCS11"
   name "acdisp11tool"
   desc "ACDIS PKCS11 Tool"

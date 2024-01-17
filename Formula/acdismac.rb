34d0105aae8e6c743c719a83b4890bb3570a909a
AustriaCard <office@austriacard.at>
2024-01-17 08:32:19 +0100

    Upated Formula

--- Formula/acdismac.rb
+++ Formula/acdismac.rb
@@ -1,12 +1,12 @@
 class Acdismac < Formula
   desc "ACDIS PKCS11 Mac"
   homepage "https://www.austriacard.at"
-  url "https://github.com/MarkusPunz12/ACDIS-PKCS11/releases/download/1.4.0/mac-p11module-1.4.0.tar.gz"
-  sha256 "5641791035563bd209999c4121ab3c53f126f3cf8c73e01b72da01a6437eb53e"
+  url "https://github.com/MarkusPunz12/ACDIS-PKCS11/releases/download/1.4.1/mac-p11module-1.4.1.tar.gz"
+  sha256 "11d0c3f8cbd9e3205177ba26218e8ce196767966fea6240a77940357c2a69115"
 
   bottle do
-    root_url "https://github.com/MarkusPunz12/homebrew-acdismac/releases/download/acdismac-1.4.0"
-    sha256 monterey: "a37f3acc1dde4e0a37e7b2eb9bb20d33021a3055d0b11e19688583e24e8ac6b7"
+    root_url "https://github.com/MarkusPunz12/homebrew-acdismac/releases/download/acdismac-1.4.1"
+    sha256 monterey: "e730d4307897ef4d55e8fc250417999c09d3363a2a80e60ec4f2304e18688bd9"
   end
   on_macos do
     def install

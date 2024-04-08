e3eac9fb4cef844ce7c06da3039424834fc8901c
AustriaCard <office@austriacard.at>
2024-04-08 12:58:03 +0200

    Updated Cask

--- Casks/acdisp11manager.rb
+++ Casks/acdisp11manager.rb
@@ -1,7 +1,7 @@
 cask "acdisp11manager" do
-  version "1.4.1"
-  sha256 "7928a8e0c34ad49a4ed1e3db9c443d978c2ceec3a15929dd51d3626aff720d90"
-  url "https://github.com/MarkusPunz12/ACDIS-PKCS11/releases/download/1.4.1/mac-p11manager-1.4.1.tar.gz", verified: "github.com/MarkusPunz12/ACDIS-PKCS11"
+  version "1.6.0"
+  sha256 "eafb51f0d28c92598a6954b43bad6914a7cd3c5b16cd54e5f9d80e3e2e37f65f"
+  url "https://github.com/MarkusPunz12/ACDIS-PKCS11/releases/download/1.6.0/mac-p11manager-1.6.0.tar.gz", verified: "github.com/MarkusPunz12/ACDIS-PKCS11"
   name "acdisp11manager"
   desc "ACDIS PKCS11 Manager"
   homepage "https://www.austriacard.at"

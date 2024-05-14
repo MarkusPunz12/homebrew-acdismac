1010778967862327d27f2c326253c7d37e553939
AustriaCard <office@austriacard.at>
2024-05-14 09:28:33 +0200

    Updated Cask

--- Casks/acdisctk.rb
+++ Casks/acdisctk.rb
@@ -1,6 +1,6 @@
 cask "acdisctk" do
   version "1.6.0"
-  sha256 "2932034a793356ffc09924f73b4fc2a69955037fa71a8a45352c6e708eff5045"
+  sha256 "ddbd586c0ad4ad40ccd479c40559c58447e5ada881a1d1542d5094bd99fdb62f"
   url "https://github.com/MarkusPunz12/ACDIS-CryptoTokenKit/releases/download/1.6.0/mac-ctk-1.6.0.tar.gz", verified: "github.com/MarkusPunz12/ACDIS-CryptoTokenKit"
   name "acdisctk"
   desc "ACDIS CryptoTokenKit"
